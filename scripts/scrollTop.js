c11769a520d08a87531bc1c007b738cbb7ecd2e2
AlessandroRomanelli <aleromrod@gmail.com>
2017-01-14 14:01:03 +0100

    Added scrollTop.js to hide floater until scrolldown

---
+++ scripts/scrollTop.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(window).on("scroll", function (){
+    var floater = $("#floater")
+    var activeClass = "show"
+    var scroll = window.scrollY
+    if (window.scrollY > 900) {
+      floater.addClass(activeClass);
+    } else {
+      floater.removeClass(activeClass);
+    }
+    console.log(window.scrollY);
+  });
+});
